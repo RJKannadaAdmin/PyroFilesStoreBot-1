aa8b7c6f5768611373aace1db67d1e0aab8e6bb4
RJKannadaAdmin <83166159+RJKannadaAdmin@users.noreply.github.com>
2021-04-29 22:59:11 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -16,18 +16,13 @@ class Config(object):
 This is Permanent Files Store Bot!
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 🤖 **My Name:** [Files Store Bot](https://t.me/{BOT_USERNAME})
-📝 **Language:** [Python3](https://www.python.org)
-📚 **Library:** [Pyrogram](https://docs.pyrogram.org)
-📡 **Hosted on:** [Heroku](https://heroku.com)
-🧑🏻‍💻 **Developer:** @AbirHasan2005
-👥 **Support Group:** [Linux Repositories](https://t.me/linux_repo)
-📢 **Updates Channel:** [Discovery Projects](https://t.me/Discovery_Updates)
+👥 **Support Group:** [Click Here](https://t.me/Kannada_HD_Films_Request)
+📢 **Updates Channel:** [Click Here](https://t.me/Kannada_Film_HD)
 """
 	ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 **Developer:** @AbirHasan2005
+**Support Group:** @Kannada_HD_FilmsRequest
 Developer is Super Noob. Just Learning from Official Docs. Please Donate the developer for Keeping the Service Alive.
 Also remember that developer will Delete Adult Contents from Database. So better don't Store Those Kind of Things.
-[Donate Now](https://www.paypal.me/AbirHasan2005) (PayPal)
 """
 	HOME_TEXT = """
 Hi, [{}](tg://user?id={})\n\nThis is Permanent **File Store Bot**.
